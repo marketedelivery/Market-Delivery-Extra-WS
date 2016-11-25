8f29ab0078e49131f5bee1f694ed1252b10f5cec
José Clebson <j.clebson@gmail.com>
2016-11-25 19:04:44 -0300

    algumas alterações

---
+++ src/main/java/br/com/marketedelivery/teste/Teste.java
@@ -0,0 +1,19 @@
+package br.com.marketedelivery.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Teste {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("db_extra");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		et.commit();
+		em.close();
+		emf.close();
+	}
+}
